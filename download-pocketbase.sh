fcbe724bf2427a7fd915f5fea93791caa92bc6fa
Victorio Berra <2934507+VictorioBerra@users.noreply.github.com>
2025-05-20 18:36:20 +0000

    Script cleanup

--- download-pocketbase.sh
+++ download-pocketbase.sh
@@ -1,4 +1,14 @@
-npx download --extract --out dest https://github.com/pocketbase/pocketbase/releases/download/v0.28.1/pocketbase_0.28.1_linux_amd64.zip
+#!/bin/bash
+
+PB_VERSION=${1:-0.28.1}
+PB_URL="https://github.com/pocketbase/pocketbase/releases/download/v${PB_VERSION}/pocketbase_${PB_VERSION}_linux_amd64.zip"
+
+npx download --extract --out dest "$PB_URL"
+
 mv dest/pocketbase ./pocketbase
+
 chmod +x ./pocketbase
+
 rm -rf dest
+
+echo "Pocketbase v${PB_VERSION} downloaded and extracted to ./pocketbase"
